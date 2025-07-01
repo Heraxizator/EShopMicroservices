c75098dcb09a89f76ecb553479e53e1be87d0d85
masuk <masuk@DESKTOP-47Q9D27>
2025-07-01 13:22:39 +1000

    Добавьте файлы проекта.

---
+++ Customer.Microservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Data
+{
+    public class ApplicationDbContext : DbContext,IApplicationDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.OpenConnection();
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Entities.Customer> Customers{ get; set; }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ Product.Microservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Microservice.Data
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.OpenConnection();
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Entities.Product> Products { get; set; }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
